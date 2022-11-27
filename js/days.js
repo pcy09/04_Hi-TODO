8e2e28a8da08c6c5d82265c3f4e45b6d39694909
pcy09 <pcy09@naver.com>
2022-11-27 22:24:38 +0900

    1차 업로드

---
+++ js/days.js
@@ -0,0 +1,34 @@
+// 최근 한달 구하기
+const days = [];
+const weekday = ["일", "월", "화", "수", "목", "금", "토"];
+const day = [];
+
+for (let i = 0; i < 31; i++) {
+	let now = new Date();
+	days[i] = new Date(now.setDate(now.getDate() - i)).getDate();
+	day[i] = weekday[now.getDay()];
+}
+let daysLength = days.length;
+let text = "";
+for (let i = 0; i < daysLength; i++) {
+	if (i === 0) {
+		text += `
+    <div class="swiper-slide" >
+      <a href="" class="checked">
+        <p>${day[i]}</p>
+        <p>${days[i]}</p>
+      </a>
+    </div>
+    `;
+	} else {
+		text += `
+  <div class="swiper-slide">
+    <a href="">
+      <p>${day[i]}</p>
+      <p>${days[i]}</p>
+    </a>
+  </div>
+  `;
+	}
+}
+document.getElementById("days").innerHTML = text;
---
+++ js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper.profile", {
+	slidesPerView: "auto",
+	freeMode: true,
+	spaceBetween: 25,
+});
+var swiper = new Swiper(".mySwiper.days", {
+	slidesPerView: "auto",
+	freeMode: true,
+	spaceBetween: 15,
+});
