d37e100c354dbdad132b878efd574b31ff8ff41a
pcy09 <pcy09@naver.com>
2022-11-30 17:19:55 +0900

    pwa설치

---
+++ sw-precache-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	staticFileGlobs: ["*.html", "css/*.css", "img/**.*", "js/*.js"],
+	stripPrefix: "app/",
+	runtimeCaching: [
+		{
+			urlPattern: /this\\.is\\.a\\.regex/,
+			handler: "networkFirst",
+		},
+	],
+};
